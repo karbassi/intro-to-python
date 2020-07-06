45e70f1cb38e95cb888ba4ddad3da6b52cbe317d
Ali Karbassi <ali@karbassi.com>
2020-07-06 11:24:17 -0500

    Initialize repository

---
+++ week-1/hello-world.py
@@ -0,0 +1,27 @@
+# print("Hello world!")
+
+# this is our person's age
+# age = 18
+# print(age)
+# print(type(age))
+
+# twice_the_age = age * 2
+# print(twice_the_age)
+# print(type(twice_the_age))
+
+
+age = input("How old are you? ")
+
+age = complex(age)
+
+# Print the input value
+print(f"You are {age} years old.")
+
+twice_the_age = age * 2
+print(f"Twice your age is {twice_the_age} years old.")
+
+
+
+
+# Print the type of 'age'
+# print(type(age))
